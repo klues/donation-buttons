445e82282dedc1db99046a865c7aa576b33e0a56
Benjamin Klaus <klues@live.de>
2018-08-13 21:21:41 +0200

    added modules for config constants and storage service for saving state to google firebase

---
+++ config.js
@@ -0,0 +1,12 @@
+window.C = {};
+
+////// can be changed: ///////
+C.projectId = 'demo-project'; //important: change this ID when copying/forking this project
+C.lowerLimit = 1;
+C.upperLimit = 210;
+C.doneAmounts = [5,6,10,300];
+C.elementsByRow = 15;
+C.backgroundImage = 'background.jpg';
+C.currency = '$';
+C.fontSize = '10px';
+/////////////////////////////
---
+++ storageService.js
@@ -0,0 +1,60 @@
+window.storageService = {};
+storageService.marked = [5, 7, 100, 101, 200];
+
+storageService.getClickedAmounts = function () {
+    return new Promise(function (resolve) {
+        getFromCloudInternal().then(function (data) {
+            storageService.marked = data;
+            resolve(storageService.marked);
+        });
+    });
+};
+
+storageService.markAmount = function (amount) {
+    storageService.marked.push(amount);
+    console.log('marked amount ' + amount + ', all marked: ' + storageService.marked);
+    saveToCloudInternal();
+};
+
+storageService.unmarkAmount = function (amount) {
+    var index = storageService.marked.indexOf(amount);
+    if (index > -1) {
+        storageService.marked.splice(index, 1);
+    }
+    console.log('unmarked amount ' + amount + ', remaining marked: ' + storageService.marked);
+    saveToCloudInternal();
+};
+
+/////////////// change this part if you want to use a different cloud service //////////////////////
+
+function initCloud() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBLsYrDhXKe7UH41rT5i7z2Lz1EMYq0WOU",
+        authDomain: "donation-buttons-2291a.firebaseapp.com",
+        databaseURL: "https://donation-buttons-2291a.firebaseio.com",
+        projectId: "donation-buttons-2291a",
+        storageBucket: "donation-buttons-2291a.appspot.com",
+        messagingSenderId: "58751055932"
+    };
+    firebase.initializeApp(config);
+}
+initCloud();
+
+function saveToCloudInternal() {
+    firebase.database().ref(getStorageRef()).set(storageService.marked);
+}
+
+function getFromCloudInternal() {
+    return new Promise(function (resolve) {
+        firebase.database().ref(getStorageRef()).once('value').then(function (snapshot) {
+            resolve(snapshot.val());
+        });
+    });
+}
+
+function getStorageRef() {
+    return '/data/' + C.projectId;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
