8ccbaa136aff623ba83d9652005e0f3398177e8b
Benjamin Klaus <klues@live.de>
2018-09-08 11:19:15 +0200

    added i18n, translations for english and german

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
+////// do not change: ///////
 window.C = {};
+C.translations = {};
 
 ////// can be changed: ///////
 C.projectId = 'demo-project'; //important: change this ID when copying/forking this project
@@ -9,6 +11,19 @@ C.elementsByRow = 15; //number of buttons in one row
 C.backgroundImage = 'img/background.jpg'; //path to the background image
 C.currency = ''; //symbol for currency to show (use '' for no currency)
 C.fontSize = '13px'; //font size of the amounts in the buttons
-C.redirectOnClickURL = 'https://www.google.at/'; //page to redirect, after a button was clicked (use '' if you do not want any redirect)
+C.redirectAfterClick = true; //if true user is redirected to an url after clicking on a donation button (see "translations --> redirectURL" below)
 C.redirectDelayMs = 1000; //delay in milliseconds, before the redirect is done (so that the user can see the change of the button to semi-transparent)
+
+C.translations['confirmDonate'] = {
+    'en' : "Do you want to donate the amount of {0}{1}? Clicking on 'OK' will mark this amount as done and redirect you to a page with payment information.",
+    'de' : 'Möchtest du wirklich den Betrag von {0}{1} spenden? Ein Klick auf "OK" wird den Betrag als "gespendet" und dich zu einer Seite mit Zahlungsinformationen weiterleiten.'
+};
+C.translations['confirmUndo'] = {
+    'en' : "Do you really want to unmark the amount {0}{1}? Please only do this if you clicked on this button before and you now decided to not spend the money.",
+    'de' : 'Möchtest du wirklich die Markierung des Betrags von {0}{1} rückgängig machen? Bitte tu das nur, wenn du zuvor auf den Button geklickt hast und nun entschieden hast den Betrag doch nicht zu spenden.'
+};
+C.translations['redirectURL'] = {
+    'en' : 'https://www.google.at/',
+    'de' : 'https://github.com/dashboard'
+};
 /////////////////////////////
